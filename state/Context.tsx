456cb88aca8f574c6f54b57fbcbb76c92750aff8
aleenayh <145587682+aleenayh@users.noreply.github.com>
2026-02-11 16:06:30 -0800

    add zustand for async state storage

--- state/Context.tsx
+++ state/Context.tsx
@@ -1,63 +1,62 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { defaultGameState } from "./default";
-import { type GameState } from "./schema";
+import { type GameState, gameStateSchema } from "./schema";
+import { useGameStore, useUnlockStore } from "./store";
 
 export function GameProvider({ children }: { children: React.ReactNode }) {
-  //TODO, retieve from storage
-  const savedState = null; //gameStateSchema.parse(JSON.parse(storedState));
-  const savedContentUnlocked = false; //TODO
+	const gameStore = useGameStore();
+	const unlockStore = useUnlockStore();
+	const savedState = gameStateSchema.parse(gameStore.gameState);
 
-  const [gameState, setGameState] = useState<GameState>(
-    savedState ?? defaultGameState(),
-  );
-  const [contentUnlocked, setContentUnlocked] = useState<boolean>(
-    savedContentUnlocked ?? false,
-  );
-  const [isReady, setIsReady] = useState<boolean>(false);
+	const [gameState, setGameState] = useState<GameState>(savedState);
+	const [contentUnlocked, setContentUnlocked] = useState<boolean>(
+		unlockStore.contentUnlocked ?? false,
+	);
+	const [isReady, setIsReady] = useState<boolean>(false);
 
-  useEffect(() => {
-    setIsReady(true);
-  }, []);
+	useEffect(() => {
+		setIsReady(true);
+	}, []);
 
-  const updateGameState = (newGameState: GameState) => {
-    setGameState(newGameState);
-    //TODO, sync to storage
-  };
+	const updateGameState = (newGameState: GameState) => {
+		setGameState(newGameState);
+		gameStore.updateGameState(newGameState);
+	};
 
-  const updateContentUnlocked = (newContentUnlocked: boolean) => {
-    setContentUnlocked(newContentUnlocked);
-    //TODO, sync to storage
-  };
+	const updateContentUnlocked = (newContentUnlocked: boolean) => {
+		setContentUnlocked(newContentUnlocked);
+		unlockStore.updateContentUnlocked(newContentUnlocked);
+	};
 
-  return (
-    <GameContext.Provider
-      value={{
-        gameState,
-        updateGameState,
-        contentUnlocked,
-        updateContentUnlocked,
-        isReady,
-      }}
-    >
-      {children}
-    </GameContext.Provider>
-  );
+	return (
+		<GameContext.Provider
+			value={{
+				gameState,
+				updateGameState,
+				contentUnlocked,
+				updateContentUnlocked,
+				isReady,
+			}}
+		>
+			{children}
+		</GameContext.Provider>
+	);
 }
 
 export function useGame() {
-  return useContext(GameContext);
+	return useContext(GameContext);
 }
 
 const GameContext = createContext<{
-  gameState: GameState;
-  updateGameState: (gameState: GameState) => void;
-  contentUnlocked: boolean;
-  updateContentUnlocked: (contentUnlocked: boolean) => void;
-  isReady: boolean;
+	gameState: GameState;
+	updateGameState: (gameState: GameState) => void;
+	contentUnlocked: boolean;
+	updateContentUnlocked: (contentUnlocked: boolean) => void;
+	isReady: boolean;
 }>({
-  gameState: defaultGameState(),
-  updateGameState: () => {},
-  contentUnlocked: false,
-  updateContentUnlocked: () => {},
-  isReady: false,
+	gameState: defaultGameState(),
+	updateGameState: () => {},
+	contentUnlocked: false,
+	updateContentUnlocked: () => {},
+	isReady: false,
 });
