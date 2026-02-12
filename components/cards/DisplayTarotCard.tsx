e63599bcbe435453af62cd6feef89806e78d00e4
aleenayh <145587682+aleenayh@users.noreply.github.com>
2026-02-12 12:42:47 -0800

    draw cards without crashing the app

--- components/cards/DisplayTarotCard.tsx
+++ components/cards/DisplayTarotCard.tsx
@@ -1,21 +1,38 @@
 import { Image, StyleSheet, Text, View } from "react-native";
-import { useDrawCard } from "./useDrawCard";
 import { CardDetails } from "./cards";
 
-export default function DisplayTarotCard() {
-  const { card, flipped, drawnIsHigher } = useDrawCard();
-
+export default function DisplayTarotCard({
+  card,
+  flipped,
+  drawnIsHigher,
+}: {
+  card: CardDetails;
+  flipped: boolean;
+  drawnIsHigher: boolean;
+}) {
   const description = getDescription(card, flipped, drawnIsHigher);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{card.name}</Text>
+      <Text style={styles.title}>
+        {card.name}
+        {flipped ? ": Reversed" : ""}
+      </Text>
       <View style={styles.imageContainer}>
-        {card.image && <Image source={card.image} style={styles.image} />}
+        {card.image && (
+          <Image
+            source={card.image}
+            style={[styles.image, flipped && styles.flippedImage]}
+          />
+        )}
       </View>
       <Text style={styles.description}>{description}</Text>
 
-      {drawnIsHigher && <Text style={styles.description}>(This will flip your story card for {card.type}.)</Text>}
+      {drawnIsHigher && (
+        <Text style={styles.description}>
+          (This will flip your story card for {card.type}.)
+        </Text>
+      )}
     </View>
   );
 }
@@ -61,20 +78,35 @@ const styles = StyleSheet.create({
     zIndex: -1,
     inset: 0,
   },
+  flippedImage: {
+    transform: [{ rotate: "180deg" }],
+  },
 });
 
-function getDescription(card: CardDetails, flipped: boolean, drawnIsHigher: boolean) {
+function getDescription(
+  card: CardDetails,
+  flipped: boolean,
+  drawnIsHigher: boolean,
+) {
   if (card.type === "majorArcana") {
     return flipped ? card.descriptionReversed : card.descriptionUpright;
-  } 
+  }
   switch (card.type) {
     case "wands":
-      return drawnIsHigher ? "Appeal to his emotions. He doubts you when your passion is performative." : "Appeal to his emotions. He believes you when you break his heart.";
+      return drawnIsHigher
+        ? "Appeal to his emotions. He doubts you when your passion is performative."
+        : "Appeal to his emotions. He believes you when you break his heart.";
     case "cups":
-      return drawnIsHigher ? "Overwhelm him with details. He doubts you when the details begin to contradict each other." : "Overwhelm him with details. He believes you when the specificity feels real.";
+      return drawnIsHigher
+        ? "Overwhelm him with details. He doubts you when the details begin to contradict each other."
+        : "Overwhelm him with details. He believes you when the specificity feels real.";
     case "pentacles":
-      return drawnIsHigher ? "Disarm him with logic. He doubts you when your argument is fallacious or circular." : "Disarm him with logic. He believes you when you confuse him with complexity.";
+      return drawnIsHigher
+        ? "Disarm him with logic. He doubts you when your argument is fallacious or circular."
+        : "Disarm him with logic. He believes you when you confuse him with complexity.";
     case "swords":
-      return drawnIsHigher ? "Intimidate him with your certainty. He doubts you when his anger rears its head in response." : "Intimidate him with your certainty. He believes you when the force of your argument beats his doubts away.";
+      return drawnIsHigher
+        ? "Intimidate him with your certainty. He doubts you when his anger rears its head in response."
+        : "Intimidate him with your certainty. He believes you when the force of your argument beats his doubts away.";
   }
 }
