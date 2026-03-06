f793c881cf98633a25a5e67486387de215bd6a56
aleenayh <145587682+aleenayh@users.noreply.github.com>
2026-03-06 11:03:38 -0800

    tower can collapse

--- state/schema.ts
+++ state/schema.ts
@@ -1,6 +1,14 @@
 import { z } from "zod";
 import { defaultGameState } from "./default";
 
+const towerSchema = z.object({
+  nextBlockPull: z.coerce.number().catch(0), //adjustment ++ to base
+  collapsed: z.boolean().catch(false),
+  rows: z.array(z.array(z.boolean().catch(true)))
+})
+
+export type Tower = z.infer<typeof towerSchema>;
+
 export const gameStateSchema = z
   .object({
     dishonesty: z.number(),
@@ -23,11 +31,7 @@ export const gameStateSchema = z
         flipped: z.boolean(),
       }),
     }),
-    tower: z.object({
-      nextBlockPull: z.coerce.number().catch(0), //adjustment ++ to base
-      collapsed: z.boolean().catch(false),
-      //more fields to come, TODO
-    }),
+    tower: towerSchema,
     deck: z.array(z.string()),
     turnCount: z.number(),
     gameOver: z.boolean().catch(false),
