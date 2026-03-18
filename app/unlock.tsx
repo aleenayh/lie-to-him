67b24088cc1fc90a8a714986507796c720766a3a
aleenayh <145587682+aleenayh@users.noreply.github.com>
2026-03-17 18:08:09 -0700

    codes

--- app/unlock.tsx
+++ app/unlock.tsx
@@ -27,7 +27,10 @@ export default function Unlock() {
 
   const checkCode = () => {
     setError(false);
-    if (code === "aleenaTestCode") {
+    if (
+      code.trim().toLowerCase() === "aleenatest" ||
+      code.trim().toLowerCase() === "deceit"
+    ) {
       updateContentUnlocked(true);
       setSuccess(true);
       setTimeout(() => {
